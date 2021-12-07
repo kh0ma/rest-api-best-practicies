b827d354a792202e40e5aa14f6b40c96ffb902c3
Oleksandr Khomenko <okhome@softserveinc.com>
2021-12-07 22:56:51 +0200

    Generate project via spring initializr.

---
+++ src/test/java/com/github/kh0ma/blog/api/BlogRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kh0ma.blog.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
